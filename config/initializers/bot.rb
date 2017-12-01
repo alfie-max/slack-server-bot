9750a628a3d5456178d36996c8ac84d9c065bef7
Alfred Dominic <mailme@alfiemax.com>
2017-12-02 00:53:52 +0530

    check if table exist before loading bot

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -1,1 +1,4 @@
-require File.join(Rails.root, 'bot/init')
+ActiveRecord::Base.connection.data_sources
+if ActiveRecord::Base.connection.data_source_exists? SlackIntegration.table_name
+  require File.join(Rails.root, 'bot/init')
+end
